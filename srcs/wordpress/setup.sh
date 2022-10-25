7e031806e5e1c819fa662a3c3a31bf452548f445
Germain Toubol <germain.toubol.pro@gmail.com>
2022-10-25 18:24:47 +0200

    First working wordpress
    need to setup env for wp-user/admin
    first blogs on config
    ssl/tls
    ...

---
+++ srcs/wordpress/setup.sh
@@ -0,0 +1,4 @@
+
+wp core install --url="gtoubol.42paris.fr" --title="Titre du Site" --admin_user="nomdutilisateur_admin" --admin_password="motdepasse_administrateur" --admin_email="votre@email.com" --path="/var/www/blog"
+
+exec $@
