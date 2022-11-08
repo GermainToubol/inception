8b6a8a421235b69515991c83abdc63db099dae98
Germain Toubol <germain.toubol.pro@gmail.com>
2022-11-08 20:42:33 +0100

    remove the blog content from the setup script and addapt the dockerfile accordingly

--- bonus/hugo/setup.sh
+++ bonus/hugo/setup.sh
@@ -11,43 +11,15 @@ fi
 
 if [ 1 "$(ls -A /var/www/public)" ]; then
 	echo "Create config file";
-	cat <<EOF > /hugo/config.toml;
-baseURL = "https://static.gtoubol.42paris.fr/"
-buildDrafts = true # To allow configuration
-defaultContentLanguage = "en"
-title = "Inception showcase"
-theme = "terminal"
+	cp /tmp/config.toml /hugo/config.toml;
 
-[Params]
-	mainSections = ["post"]
-	intro = true
-	headline = "my headline"
-	description = "an amazing description"
-	github = "https://githun.com/GermainToubol"
-	email = "germain.toubol.pro@gmail.com"
-EOF
+	echo "Create posts"
+	for file in "$(ls /tmp/content)"; do
+		hugo new "posts/${file}";
+		cp "/tmp/${file}" "/hugo/content/posts/${file}"
+	done
 
-	hugo new posts/all-about-inception.md;
-	cat <<EOF > /hugo/content/posts/all-about-inception.md;
----
-title: "All about inception"
-date: 2022-11-08T08:47:11+01:00
-draft: false
----
-# Project description
-
-The aim of this project is to build an docker infrastructure able to run a
-website and many realted services.
-
-## Mandatory
-
-At first level, we need to create 3 docker containers, each one running one of the following services:
-   - nginx
-   - wordpress
-   - mariadb
-
-EOF
-
+	echo "Generate website"
 	cd /hugo && hugo;
 	cp -r /hugo/public /var/www/
 fi
