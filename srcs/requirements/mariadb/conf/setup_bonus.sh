064ed1b9629ce78dd9b05549d6403f05645edca9
Germain Toubol <germain.toubol.pro@gmail.com>
2022-11-09 16:15:02 +0100

    mariadb: update folders, add .gitignore

---
+++ srcs/requirements/mariadb/conf/setup_bonus.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+mysqld -u root --bind-address=localhost --silent-startup > /tmp/mariastart.log 2>&1 &
+PID=$!
+until tail "/tmp/mariastart.log" | grep -qi "Version:"; do
+	sleep 0.2
+done
+
+mysql <<EOF
+create database wpdatabase;
+grant all privileges on wpdatabase.* to '${MDB_USER}'@wordpress.mandatory identified by '${MDB_PASSWD}';
+alter user 'root'@'localhost' identified by '${MDB_ROOT_PASSWD}';
+create user '${MDB_HEALTH}'@'localhost' identified by '${MDB_HEALTH_PASSWD}';
+flush privileges;
+EOF
+
+mysql --password="${MDB_ROOT_PASSWD}" <<EOF
+grant all privileges on wpdatabase.* to '${ADMINER_USER}'@adminer.mandatory identified by '${ADMINER_PASSWD}';
+flush privileges;
+EOF
+
+kill -TERM ${PID}
+chown -R mysql:mysql /var/lib/mysql;
+
+exec $@
